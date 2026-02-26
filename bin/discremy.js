214f506053c2da30c98ceaa99f095b1bdb296892
RonaldZav <100430955+RonaldZav@users.noreply.github.com>
2026-02-26 06:11:43 -0500

    fix some errors

--- bin/discremy.js
+++ bin/discremy.js
@@ -3,11 +3,15 @@
 const args = process.argv.slice(2);
 const Discremy = require('../lib/Core');
 
+process.on('unhandledRejection', (reason, p) => { console.log(reason, p); });
+process.on('uncaughtException', (err, origin) => { console.log(err, origin); });
+process.on('uncaughtExceptionMonitor', (err, origin) => { console.log(err, origin); });
+
 if (args[0] === 'start') {
     const bot = new Discremy();
     bot.connect().catch(err => {
         console.error('Error starting the bot:', err);
     });
 } else {
-    console.log('Unrecognized command. Use: powerdiscord start');
+    console.log('Unrecognized command. Use: discremy start');
 }
